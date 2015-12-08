c5b952c97e868633f4a86efb188e961df6f48a3d
unknown <github@herskovits.com>
2015-12-08 14:24:09 +0200

    added conf to __init__

--- p2whois/__init__.py
+++ p2whois/__init__.py
@@ -1,4 +1,4 @@
-from .p2whois import application
-__all__ = [ "application" ]
+from .p2whois import application, conf
+__all__ = [ "application", "conf" ]
 __version__ = "0.4"
 __version_info__ = ( 0, 4, 0 )
